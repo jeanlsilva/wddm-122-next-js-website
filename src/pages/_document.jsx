1b60675af4b03d91cc5133a0e6733458e3b712c2
jean <jeanslucasilva@gmail.comm>
2022-08-11 21:09:47 -0400

    pages config

---
+++ src/pages/_document.jsx
@@ -0,0 +1,20 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet" /> 
+                    <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;600&display=swap" rel="stylesheet" /> 
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}

b8ea693aa5a4372b8dcee2f17a9581902800f49b
jean <jeanslucasilva@gmail.comm>
2022-08-12 09:38:00 -0400

    responsiveness improvements

--- src/components/TravelTypes.jsx
+++ src/components/TravelTypes.jsx
@@ -2,10 +2,11 @@ import { Grid, Center, Image, Text, useMediaQuery, Flex, Box } from '@chakra-ui/
 
 export function TravelTypes() {
   const [minWidth1024] = useMediaQuery("(min-width: 1024px)");
-  const [maxWidth425] = useMediaQuery("(max-width: 425px)");
+  const [minWidth768] = useMediaQuery("(min-width: 768px)");
+  const [maxWidth520] = useMediaQuery("(max-width: 520px)");
 
     return (
-      maxWidth425 ? (
+      maxWidth520 ? (
         <Flex flexWrap="wrap" justify="center" mt="11px">
           <Flex flexDir="row" w="50%" justify="center" mt="27px">
               <Image src="/Ellipse.svg" alt="Cocktail" mr={5} w="8px" />
@@ -30,27 +31,29 @@ export function TravelTypes() {
         </Flex>
       ) : (
         <Grid 
-          templateColumns={minWidth1024 ? "repeat(5, 1fr)" : "repeat(2, 1fr)"} 
-          mt="114px">
+          templateColumns={minWidth1024 ? "repeat(5, 1fr)" : !maxWidth520 ? "repeat(2, 1fr)" : "repeat(1, 1fr)"} 
+          rowGap={6}
+          mt="114px"
+        >
             <Center flexDir="column">
               <Image src="/Cocktail.svg" alt="Cocktail" />
-              <Text fontSize="lg" fontWeight="600" mt={6}>nightlife</Text>
+              <Text fontSize="lg" fontWeight="600" mt={3}>nightlife</Text>
             </Center>
             <Center flexDir="column">
               <Image src="/Surf.svg" alt="Surf" />
-              <Text fontSize="lg" fontWeight="600" mt={6}>beach</Text>
+              <Text fontSize="lg" fontWeight="600" mt={3}>beach</Text>
             </Center>
             <Center flexDir="column">
               <Image src="/Building.svg" alt="Building" mr={5} />
-              <Text fontSize="lg" fontWeight="600" mt={6}>modern</Text>
+              <Text fontSize="lg" fontWeight="600" mt={3}>modern</Text>
             </Center>
             <Center flexDir="column">
               <Image src="/Museum.svg" alt="Museum" />
-              <Text fontSize="lg" fontWeight="600" mt={6}>classic</Text>
+              <Text fontSize="lg" fontWeight="600" mt={3}>classic</Text>
             </Center>
             <Center flexDir="column">
               <Image src="/Earth.svg" alt="Earth" />
-              <Text fontSize="lg" fontWeight="600" mt={6}>and more...</Text>
+              <Text fontSize="lg" fontWeight="600" mt={3}>and more...</Text>
             </Center>
           </Grid>
       )
--- src/pages/continent/[continent].jsx
+++ src/pages/continent/[continent].jsx
@@ -1,44 +1,53 @@
-import { Flex, Box, Heading, Text, Grid, GridItem, Image } from "@chakra-ui/react";
+import { Flex, Box, Heading, Text, Grid, GridItem, Image, useMediaQuery } from "@chakra-ui/react";
 import { api } from "../../../services/api";
 import Header from "../../components/Header";
 
 export default function Continent({ continent }) {
+    const [mw1024] = useMediaQuery("(min-width: 1024px)");
+    const [mw600] = useMediaQuery("(min-width: 600px)");
+
     return (
         <>
             <Header />
             <Flex w="100%" h={500} bg={`url('${continent.image}') no-repeat`} bgSize="cover" align="flex-end" justify="center">
-                <Box w={1160} mb="59px">
+                <Box w={mw1024 ? 1160 : '90%'} mb="59px">
                     <Heading fontSize="5xl" fontWeight="600" color="#fff">{continent.title}</Heading>
                 </Box>
             </Flex>
-            <Flex w={1160} m="0 auto" mt="80px">
-                <Box w="50%">
+            <Flex w={mw1024 ? 1160 : '90%'} m="0 auto" mt="80px" flexDir={mw1024 ? 'row' : 'column'} alignItems={mw1024 ? 'flex-start' : 'center'}>
+                <Box w={mw1024 ? "50%" : "90%"}>
                     <Text fontSize="2xl" textAlign="justify" lineHeight="36px">
                         {continent.description}
                     </Text>
                 </Box>
-                <Flex w="50%" align="center" justify="space-around">
+                <Flex w={mw1024 ? "50%" : "90%"} align="center" justify={mw600 ? "space-around" : "space-between"} mt={mw600 ? 0 : "20px"}>
                     <Box>
-                        <Heading fontSize="5xl" fontWeight="600" color="#ffba08">{continent.countries}</Heading>
-                        <Text fontSize="lg" fontWeight="600">countries</Text>
+                        <Heading fontSize="5xl" fontWeight="600" color="#ffba08" textAlign={mw600 ? "left" : "center"}>
+                            {continent.countries}
+                        </Heading>
+                        <Text fontSize="lg" fontWeight="600" textAlign={mw600 ? "left" : "center"}>countries</Text>
                     </Box>
                     <Box>
-                        <Heading fontSize="5xl" fontWeight="600" color="#ffba08">{continent.languages}</Heading>
-                        <Text fontSize="lg" fontWeight="600">languages</Text>
+                        <Heading fontSize="5xl" fontWeight="600" color="#ffba08" textAlign={mw600 ? "left" : "center"}>
+                            {continent.languages}
+                        </Heading>
+                        <Text fontSize="lg" fontWeight="600" textAlign={mw600 ? "left" : "center"}>languages</Text>
                     </Box>
                     <Box>
-                        <Heading fontSize="5xl" fontWeight="600" color="#ffba08">{continent.cidades100.length}</Heading>
-                        <Text fontSize="lg" fontWeight="600">cities +100</Text>
+                        <Heading fontSize="5xl" fontWeight="600" color="#ffba08" textAlign={mw600 ? "left" : "center"}>
+                            {continent.cidades100
+                        .length}</Heading>
+                        <Text fontSize="lg" fontWeight="600" textAlign={mw600 ? "left" : "center"}>cities +100</Text>
                     </Box>
                 </Flex>
             </Flex>
-            <Flex w={1160} m="0 auto" mt="80px" mb="35px" flexDir="column">
+            <Flex w={mw1024 ? 1160 : '80%'} m="0 auto" mt="80px" mb="35px" flexDir="column">
                 <Heading fontSize="4xl" fontWeight="500">Cities +100</Heading>
-                <Grid templateColumns="repeat(4, 1fr)" gap={12} mt="40px">
+                <Grid templateColumns={mw1024 ? "repeat(4, 1fr)" : mw600 ? "repeat(2, 1fr)" : "repeat(1, 1fr)"} gap={12} mt="40px">
                     {continent.cidades100.map(city => {
                         return (
                             <GridItem key={city.id}>
-                                <Image src={city.image} alt="city" h="171.63px" w="100%"/>
+                                <Image src={city.image} alt="city" h={mw600 ? "171.63px" : "auto"} w="100%"/>
                                 <Flex justify="space-between" align="center" border="1px solid #ffba08" borderTop="0" padding="25px 24px">
                                     <Box>
                                         <Heading fontFamily="Barlow" fontSize="xl">{city.name}</Heading>
